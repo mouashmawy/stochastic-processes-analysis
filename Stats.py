ef7143083772714fc2374d76512e367450ce6e47
Moustafa Ashamwy <94708036+mouashmawy@users.noreply.github.com>
2023-01-12 00:47:42 +0200

    continue

--- Stats.py
+++ Stats.py
@@ -6,13 +6,14 @@ class Stats:
     def __init__(self,ensemble,time):
         self.processes = ensemble
         self.time = time
-        
-        
+        self.lenP = len(self.processes)        
+        self.lenT = len(self.time) 
+               
     def plotMsamples(self,number):
-        for n in range(len(self.processes)):
+        for n in range(number):
             plt.plot(self.time,self.processes[n])
         plt.show()
-        
+    
     def plotSampleN(self,number):
         plt.plot(self.time,self.processes[number])
         plt.show()
@@ -25,9 +26,20 @@ class Stats:
         plt.show()
         
     def plotACFbetween(self, ith, jth):
-        pass
+        I = self.processes[:self.lenP, ith]
+        J = self.processes[:self.lenP, jth]
+        return sum(I*J)/self.lenP
     
     def calcMeanOf(self,n):
         mean = np.mean(self.processes[n])
         return mean
+    
+    def calcTimeACF(self):
+        pass
+    
+    def plotPSD(self):
+        pass 
+    
+    def calcAvgPower(self):
+        pass
         
